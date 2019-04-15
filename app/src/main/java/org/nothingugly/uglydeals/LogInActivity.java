24541fde8c12fe85855efea4fae29b18c6f5086c
Navindu Dissanayake <n.dissanayake09@gmail.com>
2019-04-15 18:24:52 +0600

    Added login function, Progress bar, log out button

--- app/src/main/java/org/nothingugly/uglydeals/LogInActivity.java
+++ app/src/main/java/org/nothingugly/uglydeals/LogInActivity.java
@@ -1,11 +1,21 @@
 package org.nothingugly.uglydeals;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LogInActivity extends AppCompatActivity {
 
@@ -13,14 +23,20 @@ public class LogInActivity extends AppCompatActivity {
     private EditText editTextLoginEmail;
     private EditText editTextLoginPassword;
 
-    //Initiaing Button components
+    //Initiating Button components
     private Button buttonLoginLogin;
     private Button buttonLoginRegister;
+
+    private ProgressBar progressBarLogin;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mAuth = FirebaseAuth.getInstance();
 
         //Assigning the EditText to the View components
         editTextLoginEmail = (EditText) findViewById(R.id.editTextLoginEmail);
@@ -29,6 +45,42 @@ public class LogInActivity extends AppCompatActivity {
         //Assigning the Buttons to the View Components
         buttonLoginLogin = (Button) findViewById(R.id.buttonLoginLogin);
         buttonLoginRegister = (Button) findViewById(R.id.buttonLoginRegister);
+
+        progressBarLogin  =  (ProgressBar) findViewById(R.id.progressBarLogin);
+
+        //When Login button is clicked
+        buttonLoginLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String email = editTextLoginEmail.getText().toString();
+                String password = editTextLoginPassword.getText().toString();
+
+                if((!TextUtils.isEmpty(email)) && !TextUtils.isEmpty(password))
+                {
+                    progressBarLogin.setVisibility(View.VISIBLE);
+
+                    mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful())
+                            {
+                                sendToMain();
+                            }
+
+                            else
+                            {
+                                String error = task.getException().getMessage();
+                                Toast.makeText(LogInActivity.this, error, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+
+                }
+
+            }
+        });
+
 
         //When Register button is clicked
         buttonLoginRegister.setOnClickListener(new View.OnClickListener() {
@@ -45,4 +97,25 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
+
+    //Whenever this activity is started
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Takes the current user
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //If there is a user logged in, Go to MainAtivity.
+        if (currentUser != null)
+        {
+            sendToMain();
+        }
+    }
+
+    private void sendToMain() {
+        Intent mainIntent = new Intent(this, MainActivity.class);
+        startActivity(mainIntent);
+        finish();
+    }
 }
--- app/src/main/java/org/nothingugly/uglydeals/MainActivity.java
+++ app/src/main/java/org/nothingugly/uglydeals/MainActivity.java
@@ -5,10 +5,19 @@ import android.support.design.internal.BottomNavigationMenu;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigation;
+    private Button buttonLogout;
+
+    private FirebaseAuth mAuth;
 
 
     @Override
@@ -16,11 +25,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initialise Firebase app
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+
+        buttonLogout = (Button) findViewById(R.id.buttonLogout);
         bottomNavigation = findViewById(R.id.bottomNavigation);
 
+        //Logout Button pressed
+        buttonLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                //Retrieves current user and signs out.
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                mAuth.signOut();
 
-       Intent intent = new Intent( this, LogInActivity.class);
-        startActivity(intent);
+                sendToLogin();
+            }
+        });
     }
+
+    //When ever MainActivity is started
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //Retrieve the current logged in user
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        //If not user is logged in
+        if(currentUser == null)
+
+        //Open loginActivity to login
+        {
+            sendToLogin();
+        }
+    }
+
+    //Send to LoginActivity
+    private void sendToLogin()
+    {
+        Intent loginintent = new Intent(this ,LogInActivity.class);
+        startActivity(loginintent);
+        finish();
+    }
+
+
 }
