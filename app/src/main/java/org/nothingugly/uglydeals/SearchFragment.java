9a5fec9524bc2ffbca6d70e82f04672963f7b723
Navindu <n.dissanayake09@gmail.com>
2019-05-02 00:00:38 +0600

    added search function, but pictures dont show

--- app/src/main/java/org/nothingugly/uglydeals/SearchFragment.java
+++ app/src/main/java/org/nothingugly/uglydeals/SearchFragment.java
@@ -3,15 +3,42 @@ package org.nothingugly.uglydeals;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SearchFragment extends Fragment {
+
+    private EditText editTextSearchSearch;
+    private ImageButton imageButtonSearch;
+    private RecyclerView recyclerViewSearchResults;
+
+    private SearchRecyclerAdapter searchRecyclerAdapter;
+    private List<Deal> searchedDeals;
+
+    private FirebaseFirestore mFirestore;
+
+
 
 
     public SearchFragment() {
@@ -20,10 +47,79 @@ public class SearchFragment extends Fragment {
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+
+        editTextSearchSearch =(EditText) view.findViewById(R.id.editTextSearchSearch);
+        imageButtonSearch =  (ImageButton) view.findViewById(R.id.imageButtonSearch);
+        recyclerViewSearchResults= (RecyclerView) view.findViewById(R.id.recyclerViewSearchResults);
+
+
+        searchedDeals = new ArrayList<>();
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(),LinearLayoutManager.VERTICAL,false);
+
+        searchRecyclerAdapter = new SearchRecyclerAdapter(searchedDeals);
+
+
+        recyclerViewSearchResults.setLayoutManager(linearLayoutManager);
+        recyclerViewSearchResults.setAdapter(searchRecyclerAdapter);
+
+
+        imageButtonSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String searchText = editTextSearchSearch.getText().toString();
+
+
+               mFirestore.collection("partners").whereEqualTo("name",searchText).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                   @Override
+                   public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                       for (DocumentChange documentChange: queryDocumentSnapshots.getDocumentChanges())
+                       {
+                           if (documentChange.getType() == DocumentChange.Type.ADDED)
+                           {
+                               DocumentSnapshot doc = documentChange.getDocument();
+                               String id = doc.getId();
+
+                               mFirestore.collection("deals").whereEqualTo("partnerID",id).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                                   @Override
+                                   public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+
+                                       for (DocumentChange documentChange : queryDocumentSnapshots.getDocumentChanges()){
+                                            if(documentChange.getType() == DocumentChange.Type.ADDED){
+
+                                                Deal deal = documentChange.getDocument().toObject(Deal.class);
+
+                                                searchedDeals.add(deal);
+
+                                                searchRecyclerAdapter.notifyDataSetChanged();
+
+
+                                            }
+                                       }
+                                   }
+                               });
+                           }
+                       }
+                   }
+               });
+
+            }
+        });
+
+
+        return view;
     }
+
 
 }
---
+++ app/src/main/java/org/nothingugly/uglydeals/SearchRecyclerAdapter.java
@@ -0,0 +1,131 @@
+package org.nothingugly.uglydeals;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+public class SearchRecyclerAdapter extends RecyclerView.Adapter<SearchRecyclerAdapter.ViewHolder> {
+    public List<Deal> dealList;
+    public FirebaseFirestore mFirestore;
+
+    public Context context;
+
+    public SearchRecyclerAdapter(List<Deal> dealList) {
+
+        this.dealList = dealList;
+
+    }
+
+    @NonNull
+    @Override
+    public SearchRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.single_search_item,viewGroup,false);
+        mFirestore = FirebaseFirestore.getInstance();
+        context = viewGroup.getContext();
+        return new SearchRecyclerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SearchRecyclerAdapter.ViewHolder viewHolder, int i) {
+
+        //Retrieve name of the deal
+        String  name = dealList.get(i).getName();
+        //Set the name of the deal to the viewholder
+        viewHolder.setName(name);
+
+        //Retrieve the image URL of the current deal at index
+        String imageURL = dealList.get(i).getDealPhoto();
+        //Set the URL to the viewHolder
+        viewHolder.setImage(imageURL);
+
+        //Taking the restaurant ID from the deal
+        final String restaurantId = dealList.get(i).getPartnerID();
+
+
+        //Getting the corresponding document for the partner ID
+        DocumentReference temp = mFirestore.collection("partners").document(restaurantId);
+
+        //Get Document Snapshot
+        temp.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+
+                //If document snap recieved successfully.
+                if (task.isSuccessful()){
+                    DocumentSnapshot restaurantDocument = task.getResult();
+                    if(restaurantDocument.exists()){
+                        String restaurantName = restaurantDocument.get("name").toString();
+
+                        //Set the name on the view holder
+                        viewHolder.setRestaurantName(restaurantName);
+
+                        String restaurantLogo = (String) restaurantDocument.get("restaurantLogo").toString();
+                        viewHolder.setLogo(restaurantLogo);
+
+                    }
+
+                }
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dealList.size();
+
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private View mView;
+
+        private TextView name;
+        private ImageView image;
+        private TextView restaurantName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+
+
+        public void setName(String nametext){
+
+            name = mView.findViewById(R.id.textViewSearchItemName);
+            name.setText(nametext);
+        }
+
+        public void setImage(String imageURL) {
+            image = mView.findViewById(R.id.imageViewSearchItem);
+            Glide.with(context).load(imageURL).into(image);
+        }
+        public void setLogo(String imageURL) {
+            image = mView.findViewById(R.id.imageViewRestaurantLogo);
+            Glide.with(context).load(imageURL).into(image);
+        }
+
+
+        public void setRestaurantName(String restName){
+            restaurantName = mView.findViewById(R.id.textViewSearchItemRestaurant);
+            restaurantName.setText(restName);
+        }
+    }
+}
+
+
+
